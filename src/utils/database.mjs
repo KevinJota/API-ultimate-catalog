54f75140cbda1ef92dde287008570c90b80d1ad2
Jardel-Monte <JardelLima23@gmail.com>
2024-06-11 10:03:08 -0300

    Add files via upload

--- src/utils/database.mjs
+++ src/utils/database.mjs
@@ -1,12 +1,15 @@
 import mongoose from 'mongoose';
 
+// URI de conexão com o banco de dados MongoDB
 const URI = 'mongodb+srv://guzmanalaca:wHPMbWOHuxx1RIac@catalog.q59852s.mongodb.net/';
 
+// Função para estabelecer a conexão com o banco de dados
 const databaseConnection = async () => {
+    // Se não houver uma conexão global do Mongoose, estabelece uma nova conexão
     if (!global.mongoose) {
-        mongoose.set('strict', false);
-        global.mongoose = await mongoose.connect(URI);
+        mongoose.set('strict', false); // Desabilita o modo "strict" do Mongoose para evitar erros desnecessários
+        global.mongoose = await mongoose.connect(URI); // Conecta-se ao banco de dados usando a URI fornecida
     }
 }
 
-export default databaseConnection; // Certifique-se de exportar a função como 'default'
+export default databaseConnection;
