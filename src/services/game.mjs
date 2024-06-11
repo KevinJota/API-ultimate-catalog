9dd050cc747adca192188ebea18d72277155962c
Jardel-Monte <JardelLima23@gmail.com>
2024-06-11 10:02:27 -0300

    Add files via upload

--- src/services/game.mjs
+++ src/services/game.mjs
@@ -1,14 +1,15 @@
 import databaseConnection from '../utils/database.mjs';
 import Game from '../models/game.mjs';
 
+// Função para encontrar e retornar todos os jogos contidos no banco de dados
 export const gamesList = async () => {
     await databaseConnection();
     const games = await Game.find();
     return games;
 }
 
+// Função para criar um novo jogo no banco de dados com os dados fornecidos
 export const createGame = async (game) => {
-    //console.log(jogo);
     await databaseConnection();
     const createdGame = await Game.create(game);
     return createdGame;
