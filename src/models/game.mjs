f85de500357b634751104f1b742fba9f54348e54
Jardel-Monte <JardelLima23@gmail.com>
2024-06-11 10:02:03 -0300

    Add files via upload

--- src/models/game.mjs
+++ src/models/game.mjs
@@ -1,14 +1,16 @@
 import mongoose from 'mongoose';
 
+// Definindo o modelo da API
 const GameSchema = new mongoose.Schema({
     title: { type: String },
     description: { type: String },
     imageUrl: { type: String },
     trailerUrl: { type: String },
     anoLancamento: { type: String },
-    genero: { type: String},
-    classificacaoEtaria: { type: String},
+    genero: { type: String },
+    classificacaoEtaria: { type: String },
     plataforma: { type: String }
-}, { timestamps: true });
+}, { timestamps: true }); // Adiciona automaticamente campos de timestamp (createdAt e updatedAt)
 
+// Exporta o modelo de jogo. Se já existir um modelo Game, utiliza-o, caso contrário, cria um novo.
 export default mongoose.models.Game || mongoose.model('Game', GameSchema);
